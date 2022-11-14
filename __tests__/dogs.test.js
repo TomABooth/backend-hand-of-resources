0a4b359f8f2bce062aa40a303df8d8e4a9107f7e
Tom Booth <tombeezle@gmail.com>
2022-11-14 10:57:06 -0800

    test 1 passing

--- __tests__/dogs.test.js
+++ __tests__/dogs.test.js
@@ -10,6 +10,33 @@ describe('dogs routes', () => {
   it('should GET list of dogs', async () => {
     const res = await request(app).get('/dogs');
     expect(res.status).toBe(200);
-    expect(res.body).toMatchInlineSnapshot();
+    expect(res.body).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "age": 4,
+          "breed": "Mutt",
+          "id": "1",
+          "name": "Marvin",
+        },
+        Object {
+          "age": 14,
+          "breed": "Lab",
+          "id": "2",
+          "name": "Max",
+        },
+        Object {
+          "age": 16,
+          "breed": "Schnauzer",
+          "id": "3",
+          "name": "Ruby",
+        },
+        Object {
+          "age": 7,
+          "breed": "Kelpie",
+          "id": "4",
+          "name": "Bently",
+        },
+      ]
+    `);
   });
 });
