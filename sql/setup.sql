7050bd11e3bcb4f533d050689b41ef52faef6286
Tom Booth <tombeezle@gmail.com>
2022-11-14 10:48:46 -0800

    setup first data table, writing first test next

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,19 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS dogs;
+
+CREATE TABLE dogs (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR,
+    age  INT,
+    breed VARCHAR
+);
+
+INSERT INTO dogs (
+    name, age, breed
+)
+VALUES
+('Marvin', 4, 'Mutt'),
+('Max', 14, 'Lab'),
+('Ruby', 16, 'Schnauzer'),
+('Bently', 7, 'Kelpie');
