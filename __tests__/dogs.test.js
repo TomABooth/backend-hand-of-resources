ae9986b7126a4c0d11b28ab8b1d5360c68bf9767
Tom Booth <tombeezle@gmail.com>
2022-11-15 10:06:15 -0800

    added delete test

--- __tests__/dogs.test.js
+++ __tests__/dogs.test.js
@@ -78,4 +78,10 @@ describe('dogs routes', () => {
     const res = await request(app).get('/dogs/123');
     expect(res.status).toBe(404);
   });
+  it('DELETE /conventions/1 should delete #1', async () => {
+    const res = await request(app).delete('/conventions/1');
+    expect(res.status).toBe(200);
+    const getRes = await request(app).get('/conventions/1');
+    expect(getRes.status).toBe(404);
+  });
 });
