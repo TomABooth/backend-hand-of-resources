5439122da358f6f0cef957b930eda5b7d33aee4f
Tom Booth <tombeezle@gmail.com>
2022-12-03 15:30:58 -0800

    fixed issues to pass first test

--- __tests__/vcameras.test.js
+++ __tests__/vcameras.test.js
@@ -3,13 +3,40 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('video cameras routes', () => {
+describe('vcameras routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('should GET list of video cameras', async () => {
+  it.only('should GET list of vcameras', async () => {
     const res = await request(app).get('/vcameras');
     expect(res.status).toBe(200);
-    expect(res.body).toMatchInlineSnapshot();
+    expect(res.body).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "brand": "Sony",
+          "id": "1",
+          "model": "FX3",
+          "resolution": "4K",
+        },
+        Object {
+          "brand": "Sony",
+          "id": "2",
+          "model": "FX6",
+          "resolution": "6K",
+        },
+        Object {
+          "brand": "RED",
+          "id": "3",
+          "model": "KOMODO",
+          "resolution": "6K",
+        },
+        Object {
+          "brand": "Canon",
+          "id": "4",
+          "model": "R5C",
+          "resolution": "8K",
+        },
+      ]
+    `);
   });
 });
